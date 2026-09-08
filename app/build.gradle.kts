a05d166395cb84c1100ad62d7cec3e7817328ec8
IbrahimAlsaudi <ibrahimalsaudi9@gmail.com>
2026-09-08 21:22:31 +0300

    Added Api key to local.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,16 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.serialization)
+}
+
+val localProperties = Properties().apply {
+    load(FileInputStream(rootProject.file("local.properties")))
 }
 
 android {
@@ -18,6 +25,16 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "HOTEL_API_BASE_URL",
+            "\"${localProperties.getProperty("HOTEL_API_BASE_URL")}\""
+        )
+    }
+
+    buildFeatures{
+        buildConfig = true
     }
 
     buildTypes {
